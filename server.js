787e19dc492e8be1cb0675930ff408738a4b0091
Sam Sullivan <samsullivan09@gmail.com>
2017-03-22 17:49:01 -0500

    Listen on port 80 for Resin.

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express()
 
 app.use(require('./controllers'))
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000.')
+app.listen(80, function () {
+  console.log('Listening on port 80.')
 })
