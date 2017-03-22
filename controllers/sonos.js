d03edd7b3b6f8d820d9bb7c63e0455fc70611c46
Sam Sullivan <samsullivan09@gmail.com>
2017-03-22 16:48:11 -0500

    Use middleware to return 500 if no Sonos devices.

--- controllers/sonos.js
+++ controllers/sonos.js
@@ -4,8 +4,13 @@ var router = express.Router()
 var SonosSystem = require('sonos-discovery')
 var discovery = new SonosSystem()
 
+router.use(function (req, res, next) {
+  if (discovery.zones.length) next()
+  res.status(500).send('No Sonos speakers detected.')
+})
+
 router.get('/', function (req, res) {
-  res.send('Sonos: ' + discovery.zones.length + ' speakers found.')
+  res.send(discovery.zones.length + ' Sonos speaker(s) detected.')
 })
 
 module.exports = router
