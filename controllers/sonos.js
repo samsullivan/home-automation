52d68b30deabcbff43b4fc9b777f1d3634ffa6e0
Sam Sullivan <samsullivan09@gmail.com>
2017-03-22 18:38:41 -0500

    Fix header issue with Sonos middleware.

--- controllers/sonos.js
+++ controllers/sonos.js
@@ -3,8 +3,11 @@ var router = express.Router()
 var Sonos = require('../models/sonos')
 
 router.use(function (req, res, next) {
-  if (Sonos.hasSpeakers()) next()
-  res.status(500).send('No Sonos speakers detected.')
+  if (!Sonos.hasSpeakers()) {
+    res.status(500).send('No Sonos speakers detected.')
+  }
+
+  next()
 })
 
 router.get('/', function (req, res) {
