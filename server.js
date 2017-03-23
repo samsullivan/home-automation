f716fbe03cf6e044230a3863e58189bbd5f9f5fc
Sam Sullivan <samsullivan09@gmail.com>
2017-03-22 19:12:47 -0500

    Install and utilize body-parser.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
 app.use(require('./controllers'))
 
 app.listen(80, function () {
