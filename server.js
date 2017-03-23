48696e0a71d2f9d0f2185afe4e1d6f540496f618
Sam Sullivan <samsullivan09@gmail.com>
2017-03-22 19:21:00 -0500

    Move body-parser to avoid unhandled non-JSON errors.

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var express = require('express')
 var app = express()
 
-var bodyParser = require('body-parser')
-app.use(bodyParser.json())
-
 app.use(require('./controllers'))
 
 app.listen(80, function () {
